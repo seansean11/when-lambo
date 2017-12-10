c1b1892d8fef16f489f706973ab514d02ced4011
Sean Michael <seankmichael@gmail.com>
2017-12-09 17:29:32 -0700

    configure migrations and database seeding

--- db/migrations/20171207224305_initialize_tables.js
+++ db/migrations/20171207224305_initialize_tables.js
@@ -1,15 +1,33 @@
-
 exports.up = (knex, Promise) =>
   Promise.all([
     knex.schema.createTable('users', (table) => {
-      table.increments('id').primary();
+      table.increments('id');
       table.string('first_name');
       table.string('last_name');
       table.string('password');
       table.string('email');
       table.timestamps();
+    }),
+    knex.schema.createTable('transactions', (table) => {
+      table.increments('id');
+      table.integer('user_id').references('id').inTable('users');
+      table.decimal('USD', 18, 2).defaultTo(0);
+      table.decimal('BTC', 18, 18).defaultTo(0);
+      table.decimal('ETH', 18, 18).defaultTo(0);
+      table.decimal('LTC', 18, 18).defaultTo(0);
+      table.decimal('DOGE', 18, 18).defaultTo(0);
+      table.timestamps();
+    }).createTable('transactions_meta', (table) => {
+      table.increments('id');
+      table.integer('transaction_id').references('id').inTable('transactions');
+      table.string('symbol');
+      table.string('side');
+      table.string('price');
+      table.timestamps();
     })
   ]);
 
 exports.down = knex =>
-  knex.schema.dropTable('users');
+  knex.schema.dropTable('transactions_meta')
+    .dropTable('transactions')
+    .dropTable('users');
--- db/seeds/initialize_tables.js
+++ db/seeds/initialize_tables.js
@@ -1,13 +1,44 @@
+// p@ssw0rd = $2a$10$D0awwaG9c.7HXEAxMpepmeJJElKav4CW6D5mmBzWUhZUopTZNJmg6
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+const users = [
+  {
+    first_name: 'Ludovoquito',
+    last_name: 'Peluche',
+    password: '$2a$10$D0awwaG9c.7HXEAxMpepmeJJElKav4CW6D5mmBzWUhZUopTZNJmg6',
+    email: 'ludovoquito@peluche.com'
+  },
+  {
+    first_name: 'Bibi',
+    last_name: 'Peluche',
+    password: '$2a$10$D0awwaG9c.7HXEAxMpepmeJJElKav4CW6D5mmBzWUhZUopTZNJmg6',
+    email: 'bibi@peluche.com'
+  },
+  {
+    first_name: 'Junior',
+    last_name: 'Peluche',
+    password: '$2a$10$D0awwaG9c.7HXEAxMpepmeJJElKav4CW6D5mmBzWUhZUopTZNJmg6',
+    email: 'junior@peluche.com'
+  }
+];
+
+const transactions = [
+  {
+    user_id: 1,
+    USD: '1000.00'
+  },
+  {
+    user_id: 2,
+    USD: '1000.00'
+  },
+  {
+    user_id: 3,
+    USD: '1000.00'
+  }
+];
+
+
+exports.seed = knex =>
+  knex('users').del()
+    .then(() => knex.insert(users).into('users'))
+    .then(() => knex('transactions').del())
+    .then(() => knex.insert(transactions).into('transactions'));
