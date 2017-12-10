065e180635cc65231881ef5a594b32f7ec5a2cf9
Sean Michael <seankmichael@gmail.com>
2017-12-09 17:35:35 -0700

    server: create reusable libs for db and logging

--- server/lib/database.js
+++ server/lib/database.js
@@ -1,5 +1,9 @@
-const CONFIG = require('config');
-const knex = require('knex')(CONFIG.db);
+const config = require('config');
+const knex = require('knex')(config.db);
 const db = require('bookshelf')(knex);
+
+db.plugin('visibility');
+db.plugin('registry');
+db.plugin('virtuals');
 
 module.exports = db;
--- server/lib/logger.js
+++ server/lib/logger.js
@@ -1,15 +1,17 @@
 const winston = require('winston');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const transports = [
   new winston.transports.Console({
-    level: 'debug',
+    level: isDev ? 'debug' : 'info',
     json: false,
     timestamp: true,
     colorize: true,
     label: process.env.NODE_ENV,
     handleExceptions: true,
     humanReadableUnhandledException: true
-  }),
+  })
 ];
 
 const logger = new winston.Logger({ transports, exitOnError: false });
