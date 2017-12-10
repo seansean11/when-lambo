6dcc6ca584d97075e99c7acf8dff6b408182c546
Sean Michael <seankmichael@gmail.com>
2017-12-09 18:17:49 -0700

    server: ensure error handling middleware catches errors

--- server/index.js
+++ server/index.js
@@ -50,7 +50,9 @@ app.use((req, res, next) => {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use((err, req, res) => {
+  console.log('hi');
+  app.use((err, req, res, next) => {
+    console.log('hi');
     res.status(err.status || 500).json({
       message: err.message,
       error: err
@@ -60,7 +62,8 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
+  console.log('hi');
   res.status(err.status || 500).json({
     message: err.message,
     error: {}
