eb84e8e7a3e39c617093361cbfe8a6b6b918895c
Sean Michael <seankmichael@gmail.com>
2017-12-11 09:23:14 -0700

    client: make sure transactions update on successful purchase/sale

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getTickers, getBalances, postTransactions } from '../core/api-service';
+import { getTickers, getBalances, postTransactions, getTransactions } from '../core/api-service';
 import Header from './Header';
 import Overview from './Overview';
 import Trading from './Trading';
@@ -7,7 +7,7 @@ import Trading from './Trading';
 class App extends Component {
   constructor() {
     super();
-    this.state = { tickers: [], balances: [] };
+    this.state = { tickers: [], balances: [], transactions: [] };
     this.getData = this.getData.bind(this);
     this.handleTradeSubmit = this.handleTradeSubmit.bind(this);
   }
@@ -21,9 +21,10 @@ class App extends Component {
   getData() {
     Promise.all([
       getTickers(),
-      getBalances()
+      getBalances(),
+      getTransactions()
     ])
-      .then(([tickers, balances]) => this.setState({ tickers, balances }))
+      .then(([tickers, balances, transactions]) => this.setState({ tickers, balances, transactions }))
       .catch(err => console.log(err));
   }
 
@@ -34,11 +35,11 @@ class App extends Component {
   }
 
   render() {
-    const { tickers, balances } = this.state;
+    const { tickers, balances, transactions } = this.state;
     return (
       <div>
         <Header />
-        <Overview tickers={tickers} balances={balances} />
+        <Overview tickers={tickers} balances={balances} transactions={transactions} />
         <Trading tickers={tickers} balances={balances} handleTradeSubmit={this.handleTradeSubmit} />
       </div>
     );
